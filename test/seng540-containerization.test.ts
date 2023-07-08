1e4af27835ea03e1eadd24921566e468738071c1
Lee Wang <leewang@amazon.com>
2023-07-08 13:42:15 -0700

    Initial commit

---
+++ test/seng540-containerization.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Seng540Containerization from '../lib/seng540-containerization-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/seng540-containerization-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Seng540Containerization.Seng540ContainerizationStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
