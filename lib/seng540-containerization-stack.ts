23a84450f8d94c5720d88e43a3aea5bcf6835972
Lee Wang <leewang@amazon.com>
2023-07-10 16:38:36 -0700

    Set up ECS cluster and service, add Storybook and NextJS containers

--- lib/seng540-containerization-stack.ts
+++ lib/seng540-containerization-stack.ts
@@ -1,16 +1,143 @@
-import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import cdk = require('aws-cdk-lib');
+import ecs = require('aws-cdk-lib/aws-ecs');
+import ec2 = require('aws-cdk-lib/aws-ec2');
+import elbv2 = require('aws-cdk-lib/aws-elasticloadbalancingv2');
+import iam = require('aws-cdk-lib/aws-iam');
+import logs = require("aws-cdk-lib/aws-logs");
+
 
 export class Seng540ContainerizationStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+// Create Cluster
+  const vpc = new ec2.Vpc(this, 'Vpc', { maxAzs: 4 });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'Seng540ContainerizationQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+  const cluster = new ecs.Cluster(this, 'EcsCluster', { 
+    vpc: vpc,
+    containerInsights: true,
+   });
+  cluster.addCapacity('DefaultAutoScalingGroup', {
+    instanceType: ec2.InstanceType.of(ec2.InstanceClass.M3, ec2.InstanceSize.LARGE)
+  });
+
+  // Create Task Role
+  const taskRole = new iam.Role(this, "TaskRole", {
+    assumedBy: new iam.ServicePrincipal("ecs-tasks.amazonaws.com")
+  })
+
+  taskRole.addToPolicy(
+    new iam.PolicyStatement({
+      resources: ["*"],
+      actions: ["*"]
+    })
+  )
+
+  // Create Execution Role
+  const executionRole = new iam.Role(this, "ExecutionRole", {
+    assumedBy: new iam.ServicePrincipal("ecs-tasks.amazonaws.com")
+  })
+
+  executionRole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonEC2ContainerRegistryPowerUser'));
+
+  // Create Task Definition
+  const taskDefinition = new ecs.Ec2TaskDefinition(this, 'TaskDefinition', {
+    taskRole: taskRole,
+    executionRole: executionRole,
+  //   volumes: [{
+  //     name: 'nextjsVolume',
+  //     host: {
+  //       sourcePath: '../../src/frontend/nextjs'
+  //     }
+  //   },
+  //   {
+  //     name: 'storybookVolume',
+  //     host: {
+  //       sourcePath: '../../src/frontend/storybook'
+  //     }
+  //   },
+  // ],
+  });
+  taskDefinition.obtainExecutionRole()
+
+  // Create Container
+  const nextjs_container = taskDefinition.addContainer('NextJS', {
+    image: ecs.ContainerImage.fromRegistry("471717104567.dkr.ecr.us-east-1.amazonaws.com/seng540-containerization-nextjs:latest"),
+    logging: ecs.LogDriver.awsLogs({
+      streamPrefix: "app",
+      logRetention: logs.RetentionDays.ONE_WEEK,
+    }),
+    memoryReservationMiB: 256,
+  });
+  nextjs_container.addPortMappings({
+    containerPort: 3000,
+    hostPort: 3000,
+    protocol: ecs.Protocol.TCP
+  });
+  nextjs_container.addPortMappings({
+    containerPort: 9229,
+    hostPort: 9229,
+    protocol: ecs.Protocol.TCP
+  });
+  // nextjs_container.addMountPoints({
+  //   containerPath: '/app',
+  //   sourceVolume: 'nextjsVolume',
+  //   readOnly: false,
+  // });
+
+  const storybook_container = taskDefinition.addContainer('Storybook', {
+    image: ecs.ContainerImage.fromRegistry("471717104567.dkr.ecr.us-east-1.amazonaws.com/seng540-containerization-storybook:latest"),
+    logging: ecs.LogDriver.awsLogs({
+      streamPrefix: "app",
+      logRetention: logs.RetentionDays.ONE_WEEK
+    }),
+    memoryReservationMiB: 512,
+  });
+  storybook_container.addPortMappings({
+    containerPort: 6006,
+    hostPort: 80,
+    protocol: ecs.Protocol.TCP
+  });
+  // storybook_container.addMountPoints({
+  //   containerPath: '/app',
+  //   sourceVolume: 'storybookVolume',
+  //   readOnly: false,
+  // });
+
+  // Create Service
+  const service = new ecs.Ec2Service(this, "Service", {
+    cluster: cluster,
+    taskDefinition: taskDefinition,
+    desiredCount: 1,
+    minHealthyPercent: 0,
+  });
+
+  // Create ALB
+  const lb = new elbv2.ApplicationLoadBalancer(this, 'ALB', {
+    vpc: vpc,
+    internetFacing: true,
+  });
+  lb.connections.allowToAnyIpv4(ec2.Port.allTcp(), "All Out")
+  lb.connections.allowFromAnyIpv4(ec2.Port.allTcp(), "All In")
+  const listener = lb.addListener('PublicListener', { port: 6006, protocol: elbv2.ApplicationProtocol.HTTP, open: true });
+
+  // Attach ALB to ECS Service
+  listener.addTargets('ECS', {
+    port: 80,
+    protocol: elbv2.ApplicationProtocol.HTTP,
+    targets: [service.loadBalancerTarget({
+      containerName: 'Storybook',
+      containerPort: 6006
+    })],
+    // include health check (default is none)
+    healthCheck: {
+      protocol: elbv2.Protocol.HTTP,
+      interval: cdk.Duration.seconds(300),
+      timeout: cdk.Duration.seconds(5),
+    }
+  });
+
+  new cdk.CfnOutput(this, 'LoadBalancerDNS', { value: lb.loadBalancerDnsName, });
   }
 }
